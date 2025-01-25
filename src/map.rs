05b53dde569df8a444c1b5811011cd79174dd1e3
Juan Esteban Guevara <Juan.Guevara@WLAPCOING94.local>
2025-01-24 21:13:35 -0500

    feat(map.rs): Pre-calculation of styles to reduce rendering times

--- src/map.rs
+++ src/map.rs
@@ -1,7 +1,7 @@
 use embedded_graphics::{
     pixelcolor::Rgb666,
     prelude::*,
-    primitives::{PrimitiveStyleBuilder, Rectangle},
+    primitives::{PrimitiveStyle, PrimitiveStyleBuilder, Rectangle},
 };
 
 use crate::{HEIGHT, WIDTH};
@@ -14,37 +14,49 @@ const MAP: [[u8; 8]; 8] = [
     [1, 0, 0, 0, 1, 0, 0, 1],
     [1, 0, 1, 0, 0, 0, 0, 1],
     [1, 0, 1, 0, 0, 1, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 1, 1, 1],
 ];
+
+const fn grid_size() -> i32 {
+    let l1 = (WIDTH as usize / MAP.len()) as i32;
+    let l2 = (HEIGHT as usize / MAP[0].len()) as i32;
+    if l1 < l2 {
+        l1
+    } else {
+        l2
+    }
+}
 
 const BACKGROUND_COLOR: Rgb666 = Rgb666::WHITE;
 const LINE_COLOR: Rgb666 = Rgb666::BLACK;
+
+const SPACE_STYLE: PrimitiveStyle<Rgb666> = PrimitiveStyleBuilder::new()
+    .stroke_color(LINE_COLOR)
+    .stroke_width(1)
+    .fill_color(BACKGROUND_COLOR)
+    .build();
+
+const WALL_STYLE: PrimitiveStyle<Rgb666> = PrimitiveStyleBuilder::new()
+    .stroke_color(LINE_COLOR)
+    .stroke_width(1)
+    .fill_color(LINE_COLOR)
+    .build();
+
+static STYLES: [PrimitiveStyle<Rgb666>; 2] = [SPACE_STYLE, WALL_STYLE];
+
+pub const GRID_SIZE: i32 = grid_size();
 
 pub fn draw_map<D>(display: &mut D) -> Result<(), D::Error>
 where
     D: DrawTarget<Color = Rgb666>,
 {
-    let grid_size: i32 =
-        std::cmp::min(WIDTH as usize / MAP.len(), HEIGHT as usize / MAP[0].len()) as i32;
-    let space_style = PrimitiveStyleBuilder::new()
-        .stroke_color(LINE_COLOR)
-        .stroke_width(1)
-        .fill_color(BACKGROUND_COLOR)
-        .build();
-
-    let wall_style = PrimitiveStyleBuilder::new()
-        .stroke_color(LINE_COLOR)
-        .stroke_width(1)
-        .fill_color(LINE_COLOR)
-        .build();
-
     for (y, row) in MAP.iter().enumerate() {
         for (x, value) in row.iter().enumerate() {
             Rectangle::new(
-                Point::new(x as i32 * grid_size, y as i32 * grid_size),
-                Size::new(grid_size as u32 + 1, grid_size as u32 + 1),
+                Point::new(x as i32 * GRID_SIZE, y as i32 * GRID_SIZE),
+                Size::new(GRID_SIZE as u32 + 1, GRID_SIZE as u32 + 1),
             )
-            .into_styled(if *value == 0 { space_style } else { wall_style })
+            .into_styled(STYLES[*value as usize])
             .draw(display)?
         }
     }
