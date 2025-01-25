1b99a070b6fd8e47cb9fd97ffcbd529c884b9c15
Juan Esteban Guevara <Juan.Guevara@WLAPCOING94.local>
2025-01-24 21:14:42 -0500

    feat(src/player.rs): Refactored angular velocity to its own variable

--- src/player.rs
+++ src/player.rs
@@ -7,7 +7,8 @@ use embedded_graphics::{
 use embedded_graphics_simulator::sdl2::Keycode;
 
 const PLAYER_SIZE: u32 = 3;
-const PLAYER_SPEED: f32 = 24.0;
+const PLAYER_SPEED: f32 = 3.0;
+const ANGULAR_SPEED: f32 = 0.01;
 const PLAYER_COLOR: Rgb666 = Rgb666::CSS_AQUA;
 const LINE_COLOR: Rgb666 = Rgb666::CSS_INDIAN_RED;
 const PI: f32 = 3.14159265359;
@@ -31,10 +32,13 @@ impl Player {
     where
         D: DrawTarget<Color = Rgb666>,
     {
+        let x = self.px * crate::map::GRID_SIZE as f32;
+        let y = self.py * crate::map::GRID_SIZE as f32;
+
         Rectangle::new(
             Point::new(
-                (self.px.round() as u32 - PLAYER_SIZE / 2) as i32,
-                (self.py.floor() as u32 - PLAYER_SIZE / 2) as i32,
+                (x as u32 - PLAYER_SIZE / 2) as i32,
+                (y as u32 - PLAYER_SIZE / 2) as i32,
             ),
             Size::new(PLAYER_SIZE, PLAYER_SIZE),
         )
@@ -48,8 +52,8 @@ impl Player {
         let (dx, dy) = (5.0 * libm::cosf(self.theta), 5.0 * libm::sinf(self.theta));
 
         Line::new(
-            Point::new(self.px as i32, self.py as i32),
-            Point::new((self.px + dx) as i32, (self.py + dy) as i32),
+            Point::new(x as i32, y as i32),
+            Point::new((x + dx) as i32, (y + dy) as i32),
         )
         .into_styled(
             PrimitiveStyleBuilder::new()
@@ -71,9 +75,15 @@ impl Player {
         let displacement = time_moving as f32 * PLAYER_SPEED / 1000.0;
         match keycode {
             Keycode::W => self.move_player(displacement, self.theta),
-            Keycode::A => self.move_player(displacement, self.theta + PI / 2.0),
+            Keycode::A => self.move_player(displacement, self.theta - PI / 2.0),
             Keycode::S => self.move_player(displacement, self.theta + PI),
-            Keycode::D => self.move_player(displacement, self.theta - PI / 2.0),
+            Keycode::D => self.move_player(displacement, self.theta + PI / 2.0),
+            Keycode::L => {
+                self.theta = (self.theta + time_moving as f32 * ANGULAR_SPEED) % (2.0 * PI)
+            }
+            Keycode::J => {
+                self.theta = (self.theta - time_moving as f32 * ANGULAR_SPEED) % (2.0 * PI)
+            }
             _ => (),
         }
     }
